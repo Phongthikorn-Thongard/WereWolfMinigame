ad46dae74b682a754fc2733849b0cfcb3bd99717
Phongthikorn-Thongard <nongaathong@gmail.com>
2020-07-05 12:44:27 +0700

    เพิ่ม Playerdata เพื่อตรวจสอบสิ่งต่างๆของผู้เล่น

--- WereWolfMinigame/src/me/Skyblueplayer/Main.java
+++ WereWolfMinigame/src/me/Skyblueplayer/Main.java
@@ -1,11 +1,9 @@
 package me.Skyblueplayer;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
---
+++ WereWolfMinigame/src/me/Skyblueplayer/Playerdata/Playermanager.java
@@ -0,0 +1,82 @@
+package me.Skyblueplayer.Playerdata;
+
+import java.util.UUID;
+
+import org.bukkit.event.Listener;
+
+public class Playermanager implements Listener{
+	private UUID uuid;
+	private boolean ingame;
+	private boolean isdead;
+	private boolean isWolf; //หมาป่า
+	private boolean isVillager; //ชาวบ้าน
+	private boolean isWitch; //นักพยากรณ์
+	private boolean isGunner; //มือปืน
+	
+	public Playermanager(UUID uuid, boolean ingame, boolean isdead, boolean isWolf, boolean isVillager,
+			boolean isWitch, boolean isGunner) {//ใส่ค่าให้ผู้เล่นเพื่อตรวจสอบ
+		this.setUuid(uuid);
+		this.setIngame(ingame);
+		this.setIsdead(isdead);
+		this.setWolf(isWolf);
+		this.setVillager(isVillager);
+		this.setWitch(isWitch);
+		this.setGunner(isGunner);
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public boolean isIngame() {
+		return ingame;
+	}
+
+	public void setIngame(boolean ingame) {
+		this.ingame = ingame;
+	}
+
+	public boolean isIsdead() {
+		return isdead;
+	}
+
+	public void setIsdead(boolean isdead) {
+		this.isdead = isdead;
+	}
+
+	public boolean isWolf() {
+		return isWolf;
+	}
+
+	public void setWolf(boolean isWolf) {
+		this.isWolf = isWolf;
+	}
+
+	public boolean isVillager() {
+		return isVillager;
+	}
+
+	public void setVillager(boolean isVillager) {
+		this.isVillager = isVillager;
+	}
+
+	public boolean isWitch() {
+		return isWitch;
+	}
+
+	public void setWitch(boolean isWitch) {
+		this.isWitch = isWitch;
+	}
+
+	public boolean isGunner() {
+		return isGunner;
+	}
+
+	public void setGunner(boolean isGunner) {
+		this.isGunner = isGunner;
+	}
+}
